c67987bc69dbd62eff6469fda476e5427b0e4dde
Sekam Dex <sekamdex@gmail.com>
2021-04-14 10:53:47 -0500

    Added some comments

--- ejercicio-suma.js
+++ ejercicio-suma.js
@@ -1,5 +1,13 @@
+/**
+ * Finds 2 indices in a that sums t. 
+ * @param {Array} a - no Array type definition for jsDocs 
+ * @param {number} t 
+ * @returns {string}
+ */
 const findSumIndices = (a = [],t = null) => {
   if (!t || a.length === 0) return 'Provide valid arguments';
+  // Because of the 2 nested loops, runtime is O(n^2/2).
+  // Dropping non dominant terms O(n^2) 
   for (let i = 0; i < a.length; i++) {
     for (let j = 0; j <= a.length; j++) {
       if (a[i] + a[j] === t) {
