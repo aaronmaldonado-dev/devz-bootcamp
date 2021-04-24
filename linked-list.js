77d208a21568b868930125babb3b1bea21d63183
Sekam Dex <sekamdex@gmail.com>
2021-04-23 22:38:33 -0500

    remove duplicates method

--- linked-list.js
+++ linked-list.js
@@ -17,28 +17,24 @@ class LinkedList {
    * @returns {Node}
    */
   prepend (value) {
-    this.size += 1;
+    this.size ++;
     const node = new Node(value, this.head);
     this.head = node;
     if (!this.tail) this.tail = node;
     return node;
   }
   append (value) {
-    this.size += 1;
+    this.size ++;
     let current = this.head;
+    const node = new Node(value);
     if (!current) {
-      const node = this.prepend(value);
-      return node;
+      this.head = node;
+      this.tail = node;
+      return this;
     }
-    while (current) {
-      if (!current.next) {
-        const node = new Node(value);
-        current.next = node;
-        this.tail = node;
-        return node;
-      }
-      current = current.next;
-    }
+    this.tail.next = node;
+    this.tail = node;
+    return this;
   }
   fromArray(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -55,29 +51,46 @@ class LinkedList {
     }
     return arr;
   }
+  removeDuplicates () {
+    const pool = new Set();
+    let current = this.head;
+    let prev = null;
+    while (current) {
+      if (pool.has(current.value)) {
+        prev.next = current.next;        
+      } else {
+        pool.add(current.value);
+        prev = current;
+      }
+      current = current.next;
+    }
+    return this;
+  }
 }
 
 const list = new LinkedList();
 
 let n;
-n = list.append(4);
+n = list.append(1);
 n = list.append(5);
-n = list.append(9);
-n = list.append(0);
+n = list.append(80);
+n = list.append(60);
 n = list.append(5);
 n = list.append(1);
 n = list.append(2);
 
+list.removeDuplicates();
+
 const a = list.toArray();
 console.log(a);
 
-const arrayList = new LinkedList();
-const arr = [3,2,1,3,4,5];
-arrayList.fromArray(arr);
+// const arrayList = new LinkedList();
+// const arr = [3,2,1,3,4,5];
+// arrayList.fromArray(arr);
 
-let current = arrayList.head;
+// let current = arrayList.head;
 
-while (current) {
-  console.log(current.value);
-  current = current.next;
-}
+// while (current) {
+//   console.log(current.value);
+//   current = current.next;
+// }
